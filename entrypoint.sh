43978d825d29bb6a51ba70b5b07bda6576c27d80
Jesús Diéguez Fernández <jesusdf@gmail.com>
2026-02-06 11:21:12 +0100

    Add support for custom CAs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,13 @@ set -euo pipefail
 
 cd /build
 
+if [ -f cert.crt ]; then
+  echo "Adding custom certificate..."
+  mkdir -p /usr/local/share/ca-certificates/custom-ca
+  cp cert.crt /usr/local/share/ca-certificates/custom-ca/
+  update-ca-certificates
+fi
+
 if [ ! -z "${AZ_URL:-}" ]; then
   # Azure DevOps Agent build
   exit $(./agent.sh)
